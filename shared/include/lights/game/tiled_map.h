22dbdd726069d1e29cab2214034d08d2b9d2b0e2
Paul Mauviel <paulm@uneeq.com>
2025-01-05 23:46:54 -0500

    Extracting the information for adding tilemap collisions + rendering tilemap

--- shared/include/lights/game/tiled_map.h
+++ shared/include/lights/game/tiled_map.h
@@ -5,14 +5,51 @@
 #pragma once
 #include <tileson/tileson.h>
 #include <filesystem>
+#include <variant>
+#include <glm/glm.hpp>
+
+
+namespace std {
+    template <>
+    struct hash<glm::vec2> {
+        size_t operator()(const glm::vec2& pos) const {
+            return hash<float>()(pos.x) ^ (hash<float>()(pos.y) << 1);
+        }
+    };
+}
 
 namespace OZZ {
 
+    struct TileDescription {
+        enum class TileCollisionType {
+            None,
+            Rectangle,
+            Polygon
+        };
+        uint32_t TileID;
+        TileCollisionType CollisionType;
+        glm::vec2 Position;
+        std::variant<glm::vec2, std::vector<glm::vec2>> CollisionData;
+        std::tuple<glm::vec2, glm::vec2> ImageRect;
+    };
+
     struct Tileset {
-        std::string Name;
-        std::filesystem::path TexturePath;
+        std::string Name {};
+        std::filesystem::path TexturePath {};
 
         // should extract more properties from tileson::Tileset
+        // Should have list of tiles, each with their own properties
+        std::unordered_map<uint32_t, TileDescription> Tiles {};
+    };
+
+    struct TileLayer {
+        std::string Name {};
+
+        std::unordered_map<glm::vec2, uint32_t> Tiles {};
+    };
+
+    struct ObjectLayer {
+        std::string Name {};
     };
 
     class TiledMap {
@@ -25,6 +62,8 @@ namespace OZZ {
 
     private:
         std::vector<Tileset> tilesets;
+        std::unordered_map<std::string, TileLayer> layers;
+        std::unordered_map<std::string, ObjectLayer> objectLayers;
         std::unique_ptr<tson::Map> map;
     };
 } // OZZ
--- shared/src/game/tiled_map.cpp
+++ shared/src/game/tiled_map.cpp
@@ -25,37 +25,50 @@ namespace OZZ {
                 .TexturePath = tileset.get<std::string>("tileset_asset_path")
             });
 
+            auto& NewTileset = tilesets.back();
+
             // loop over tiles
             for (auto& tile : tileset.getTiles()) {
+                auto NewTileDescription = TileDescription {
+                    .TileID = tile.getId(),
+                    .CollisionType = TileDescription::TileCollisionType::None,
+                };
+
                 spdlog::info("Tile: {}", tile.getId());
                 // get collision information
                 for (const auto& collisionObject : tile.getObjectgroup().getObjects()) {
-                    spdlog::info("Collision Object: {}", collisionObject.getName());
-
                     auto objectType = collisionObject.getObjectType();
                     if (objectType == tson::ObjectType::Rectangle) {
                         spdlog::info("Collision Object is a rectangle");
                         auto rect = collisionObject.getSize();
                         auto position = collisionObject.getPosition();
                         spdlog::info("Collision Object AABB: x: {}, y: {}, width: {}, height: {}", position.x, position.y, rect.x, rect.y);
-                        // TODO: Store this
+
+                        NewTileDescription.CollisionType = TileDescription::TileCollisionType::Rectangle;
+                        NewTileDescription.Position = {position.x, position.y};
+                        NewTileDescription.CollisionData = glm::vec2(rect.x, rect.y);
                     } else if (objectType == tson::ObjectType::Polygon) {
                         spdlog::info("Collision Object is a polygon");
                         // print all points of polygon
+                        std::vector<glm::vec2> points;
                         for (const auto& point : collisionObject.getPolygons()) {
                             spdlog::info("Polygon Point: x: {}, y: {}", point.x, point.y);
+                            points.emplace_back(point.x, point.y);
                         }
+
+                        NewTileDescription.CollisionType = TileDescription::TileCollisionType::Polygon;
+                        NewTileDescription.Position = {collisionObject.getPosition().x, collisionObject.getPosition().y};
+                        NewTileDescription.CollisionData = points;
                     } else {
                         spdlog::error("Collision Object is not supported {}" , (uint8_t)collisionObject.getObjectType());
                     }
+
+                    auto NewImageRect = tile.getDrawingRect();
+                    NewTileDescription.ImageRect = {glm::vec2(NewImageRect.x, NewImageRect.y), glm::vec2(NewImageRect.width, NewImageRect.height)};
+                    spdlog::info("Image Rect: x: {}, y: {}, width: {}, height: {}", NewImageRect.x, NewImageRect.y, NewImageRect.width, NewImageRect.height);
+                    NewTileset.Tiles[tile.getId()] = NewTileDescription;
                 }
             }
-        }
-
-        // Print tilesets:
-        for (auto& tileset : tilesets) {
-            spdlog::info("Tileset: {}", tileset.Name);
-            spdlog::info("Texture Path: {}", tileset.TexturePath.string());
 
         }
 
@@ -63,15 +76,24 @@ namespace OZZ {
         for (auto& layer : map->getLayers()) {
             spdlog::info("Layer: {}", layer.getName());
             if (layer.getType() == tson::LayerType::TileLayer) {
+                auto NewTileLayer = TileLayer {
+                    .Name = layer.getName()
+                };
+
                 spdlog::info("Layer is a tile layer");
                 for (auto &tile: layer.getTileData()) {
-//                    spdlog::info("Tile: {}", tile.second->getId());
+                    auto& [x, y] = tile.first;
+                    auto tileID = tile.second->getId();
+
+                    NewTileLayer.Tiles[{x, y}] = tileID;
                 }
             }
             if (layer.getType() == tson::LayerType::ObjectGroup) {
                 spdlog::info("Layer is an object group");
                 for (auto &object: layer.getObjects()) {
-//                    spdlog::info("Object: {}", object.getName());
+                    // TODO: implement object layer
+                    // NOTE: objects can be of various types. I'll support the ones
+                    // I use for now and then expand as needed
                 }
             }
         }
