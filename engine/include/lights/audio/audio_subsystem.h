535971132c7975d11c11f1afaae14fe4737dbbc9
Paul Mauviel <ozzadar@ozzadar.com>
2025-07-14 00:01:41 -0400

    basic saw wave

--- engine/include/lights/audio/audio_subsystem.h
+++ engine/include/lights/audio/audio_subsystem.h
@@ -4,6 +4,7 @@
 
 #pragma once
 
+#include <any>
 #include <string>
 #include <vector>
 #include <memory>
@@ -30,6 +31,32 @@ namespace OZZ::lights::audio {
     public:
         void Init();
         void Shutdown();
+
+        [[nodiscard]] const std::vector<AudioDevice>& GetAudioDevices() const {
+            return audioDevices;
+        }
+
+        [[nodiscard]] bool IsInitialized() const {
+            return rtAudio != nullptr;
+        }
+
+        [[nodiscard]] bool IsValidDevice(const uint32_t deviceID) const {
+            return std::ranges::find_if(audioDevices, [deviceID](const AudioDevice& device) {
+                return device.ID == deviceID;
+            }) != audioDevices.end();
+        }
+
+        [[nodiscard]] const AudioDevice* GetAudioDevice(const uint32_t deviceID) const {
+            auto device = std::ranges::find_if(audioDevices, [deviceID](const AudioDevice& device) {
+                return device.ID == deviceID;
+            });
+            if (device != audioDevices.end()) {
+                return &(*device);
+            }
+            return nullptr;
+        }
+
+        void SelectOutputAudioDevice(bool bUseDefault = true, const uint32_t deviceID = 0);
 
     private:
         void detectAudioDevices();
