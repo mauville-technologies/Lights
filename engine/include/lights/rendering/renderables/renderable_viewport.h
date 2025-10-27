3be9ce6c91626c62ef98fde6c7335ed460692811
Paul Mauviel <p.a.mauviel@gmail.com>
2025-10-26 20:21:26 -0400

    rendering graph start!

---
+++ engine/include/lights/rendering/renderables/renderable_viewport.h
@@ -0,0 +1,16 @@
+//
+// Created by paulm on 2025-10-21.
+//
+
+#ifndef RENDERABLE_VIEWPORT_H
+#define RENDERABLE_VIEWPORT_H
+
+namespace OZZ {
+
+class RenderableViewport {
+
+};
+
+} // OZZ
+
+#endif //RENDERABLE_VIEWPORT_H
