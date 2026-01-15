d19c854fbd8b7db05cfbcb0c0eaf7c6db9fe476b
paulm <paulm@uneeq.com>
2026-01-15 00:34:39 -0500

    adding spritesheets to resource manager

--- engine/include/lights/scene/resource_manager.h
+++ engine/include/lights/scene/resource_manager.h
@@ -20,15 +20,36 @@ namespace OZZ {
 }
 
 namespace OZZ::scene {
+    struct SpritesheetFrameInformation {
+        std::string AnimationName;
+        uint32_t FrameNumber;
+        glm::ivec2 Position{};
+        glm::ivec2 Size{};
+        glm::vec2 Pivot{};
+    };
+
+    using SpritesheetAnimationFrames = std::vector<SpritesheetFrameInformation>;
+
+    struct SpritesheetInformation {
+        glm::ivec2 TextureSize;
+        std::unordered_map<std::string, SpritesheetAnimationFrames> Animations;
+    };
+
+    struct SpritesheetLocation {
+        std::filesystem::path JsonMetadata;
+        std::filesystem::path Texture;
+    };
+
     class ResourceManager {
     public:
         explicit ResourceManager();
         ~ResourceManager();
 
         std::shared_ptr<Texture> LoadTexture(const std::filesystem::path& path);
-
         std::shared_ptr<Texture> LoadTexture(std::shared_ptr<Image> image);
 
+        bool LoadSpritesheet(const SpritesheetLocation& location);
+        const std::unordered_map<std::string, SpritesheetInformation>& GetSpritesheets() const;
         /**
          * Runs the render jobs on the main thread
          */
@@ -48,6 +69,10 @@ namespace OZZ::scene {
 
         std::mutex renderJobsMutex;
         std::deque<std::function<void()>> renderJobs{};
+
+        // TODO: @paulm -- the resource manager has some weird separation of concerns at the moment. I should come back
+        // to this
+        std::unordered_map<std::string, SpritesheetInformation> spriteSheets{};
     };
 
 } // namespace OZZ::scene
--- engine/src/scene/resource_manager.cpp
+++ engine/src/scene/resource_manager.cpp
@@ -8,6 +8,8 @@
 #include "lights/util/memory_literals.h"
 
 #include <deque>
+#include <fstream>
+#include <nlohmann/json.hpp>
 #include <spdlog/spdlog.h>
 #include <utility>
 
@@ -57,6 +59,120 @@ namespace OZZ::scene {
             newTexture->UploadData(image.get());
         });
         return newTexture;
+    }
+
+    bool ResourceManager::LoadSpritesheet(const SpritesheetLocation& location) {
+        const auto& [jsonPath, texturePath] = location;
+        spdlog::info("Parsing spritesheet {}", jsonPath.string());
+        // Load json file
+        std::ifstream file(jsonPath.string());
+        nlohmann::json data = nlohmann::json::parse(file);
+
+        // get metadata
+        if (!data.contains("meta")) {
+            spdlog::error("No meta information in spritesheet");
+            return false;
+        }
+
+        const auto meta = data.at("meta");
+        if (!meta.contains("size")) {
+            spdlog::error("No size meta information in spritesheet");
+            return false;
+        }
+
+        const auto size = meta.at("size");
+        if (!size.contains("w") || !size.contains("h")) {
+            spdlog::error("No w/h meta information in spritesheet");
+            return false;
+        }
+
+        const auto sheetWidth = size.at("w").get<uint32_t>();
+        const auto sheetHeight = size.at("h").get<uint32_t>();
+        spdlog::info("Spritesheet {} has size {}x{}", jsonPath.string(), sheetWidth, sheetHeight);
+
+        constexpr auto extractFrameInformation =
+            [](nlohmann::json const& value) -> OZZ::scene::SpritesheetFrameInformation {
+            OZZ::scene::SpritesheetFrameInformation info{};
+
+            // Get the animation name
+            if (!value.contains("filename")) {
+                spdlog::error("No frame information in spritesheet");
+                return info;
+            }
+            const auto filename = value.at("filename");
+            std::string stem = std::filesystem::path(filename.get<std::string>()).stem().string();
+            const auto pos = stem.find_last_of('_');
+            info.AnimationName = stem.substr(0, pos);
+            info.FrameNumber = std::stoi(stem.substr(pos + 1));
+
+            // let's get the size now
+            if (!value.contains("frame")) {
+                spdlog::error("No frame information in spritesheet");
+                return info;
+            }
+
+            const auto frame = value.at("frame");
+            const auto x = frame.at("x").get<uint32_t>();
+            const auto y = frame.at("y").get<uint32_t>();
+            const auto frameWidth = frame.at("w").get<uint32_t>();
+            const auto frameHeight = frame.at("h").get<uint32_t>();
+
+            info.Position = {x, y};
+            info.Size = {frameWidth, frameHeight};
+
+            // and finally the pivot
+            if (!value.contains("pivot")) {
+                spdlog::info("No pivot in spritesheet");
+                return info;
+            }
+
+            const auto pivot = value.at("pivot");
+            const auto pivotX = pivot.at("x").get<float>();
+            const auto pivotY = pivot.at("y").get<float>();
+
+            info.Pivot = {pivotX, pivotY};
+            return info;
+        };
+
+        // get the frames array
+        if (!data.contains("frames")) {
+            spdlog::info("No frames in spritesheet");
+            return false;
+        }
+        const auto frames = data.at("frames");
+        if (!frames.is_array()) {
+            spdlog::info("Frames is not array");
+            return false;
+        }
+
+        std::unordered_map<std::string, OZZ::scene::SpritesheetAnimationFrames> animations{};
+        for (const auto& frame : frames) {
+
+            const auto frameInfo = extractFrameInformation(frame);
+
+            if (!animations.contains(frameInfo.AnimationName)) {
+                // create the vector
+                animations.emplace(frameInfo.AnimationName, OZZ::scene::SpritesheetAnimationFrames{});
+            }
+
+            animations[frameInfo.AnimationName].emplace_back(frameInfo);
+        }
+
+        // sort the animations by frame number
+        for (auto& unsortedFrames : animations | std::views::values) {
+            std::ranges::sort(unsortedFrames, std::less{}, &OZZ::scene::SpritesheetFrameInformation::FrameNumber);
+        }
+
+        OZZ::scene::SpritesheetInformation sheetInfo{
+            .TextureSize = {sheetWidth, sheetHeight},
+            .Animations = animations,
+        };
+        spriteSheets.emplace(std::make_pair(texturePath.string(), sheetInfo));
+        return true;
+    }
+
+    const std::unordered_map<std::string, SpritesheetInformation>& ResourceManager::GetSpritesheets() const {
+        return spriteSheets;
     }
 
     void ResourceManager::Tick() {
